c9805028d88a4faec8faec4902ad923e7fc32f34
Rye <31146412+Dual-Iron@users.noreply.github.com>
2026-05-23 00:56:44 -0400

    Update version to 5.1.0

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -5,6 +5,6 @@ namespace Fisobs
     /// <summary>
     /// Do not access this class.
     /// </summary>
-    [BepInPlugin("io.github.dual.fisobs", "Fisobs", "4.0.8")]
+    [BepInPlugin("io.github.dual.fisobs", "Fisobs", "5.1.0")]
     public class Plugin : BaseUnityPlugin {}
 }
